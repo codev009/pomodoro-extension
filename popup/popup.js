32d57c4e6d2a4aa4e97fd57bf6c239f5aa8d4fe7
codev009 <antoney007i@gmail.com>
2022-10-22 16:38:11 +0530

    add alarms, notification, service worker

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,18 @@
-const tasks = [];
+let tasks = [];
 
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
+
+chrome.storage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
+
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasks,
+  });
+}
 
 function renderTask(taskNum) {
   const taskRow = document.createElement("div");
@@ -12,6 +23,7 @@ function renderTask(taskNum) {
   text.value = tasks[taskNum];
   text.addEventListener("change", () => {
     tasks[taskNum] = text.value;
+    saveTasks();
   });
 
   const deleteBtn = document.createElement("input");
@@ -32,11 +44,13 @@ function addTask() {
   const taskNum = tasks.length;
   tasks.push("");
   renderTask(taskNum);
+  saveTasks();
 }
 
 function deleteTask(taskNum) {
   tasks.splice(taskNum, 1);
   renderTasks();
+  saveTasks();
 }
 
 function renderTasks() {
