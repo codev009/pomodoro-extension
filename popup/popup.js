68a5f15976823a5bce3136820e37f5f2ccd0abaf
codev009 <antoney007i@gmail.com>
2022-10-23 11:32:21 +0530

    add core functionality

--- popup/popup.js
+++ popup/popup.js
@@ -3,8 +3,11 @@ let tasks = [];
 function updateTime() {
   chrome.storage.local.get(["timer"], (res) => {
     const time = document.getElementById("time");
-    const minutes = 25 - Math.ceil(res.timer / 60);
-    const seconds = 60 - (res.timer % 60);
+    const minutes = `${25 - Math.ceil(res.timer / 60)}`.padStart(2, "0");
+    let seconds = "00";
+    if (res.timer % 60 != 0) {
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    }
     time.textContent = `${minutes}:${seconds}`;
   });
 }
